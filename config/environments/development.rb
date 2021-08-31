05e5ad7b5034e78fe97f5c79f8f2b7a052918937
harsh291199 <harshsuthar2911@gmail.com>
2021-08-31 12:57:14 +0530

    Update layout and build homepage of app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -36,6 +35,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 
   config.action_mailer.perform_caching = false
 
